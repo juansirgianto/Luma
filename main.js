4170b4a9ab92acb9f22b305bafdc4371a349f289
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-06-05 15:35:22 +0800

    js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+console.log("✅ main.js loaded!");
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { LumaSplatsThree } from '@lumaai/luma-web';
