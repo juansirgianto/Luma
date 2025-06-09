5871c18c82d959e335f489486d8dd474874b1629
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-06-09 09:10:29 +0800

    pleasee

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-// import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-// import * as THREE from "../node_modules/three/build/three.module.js";
-import { OrbitControls } from 'https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js';
-import { LumaSplatsThree } from 'https://unpkg.com/@lumaai/luma-web@0.2.2/dist/library/luma-web.module.js'; // jika tidak CORS error
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+import { OrbitControls } from 'https://esm.run/three@0.160.0/examples/jsm/controls/OrbitControls.js';
+import { LumaSplatsThree } from '../libs/luma-web.module.js';
+// import { LumaSplatsThree } from 'https://unpkg.com/@lumaai/luma-web@0.2.2/dist/library/luma-web.module.js'; // jika tidak CORS error
 
 
 // 🎯 Setup dasar
